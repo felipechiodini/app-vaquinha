267dc0dfbc75c74db28cbf89db4a3350ab070d93
Felipe Bona <felipechiodinibona@hotmail.com>
2023-01-21 15:37:13 -0300

    second commit

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
+import CowfundScreen from './screens/CowfundScreen';
+import ContributorScreen from './screens/ContributorScreen';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="CowfundDetails" component={CowfundDetailsScreen} />
-        // other screens
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Cowfunds" component={CowfundScreen} />
+        <Tab.Screen name="Contributors" component={ContributorScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
